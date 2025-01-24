10104dfec5b049488d79a3cd9ce4a273243de0d9
Conor <gillespie.conor@gmail.com>
2025-01-24 19:31:40 +0000

    nav bar for categories basic implementation

--- ViewModels/ProductPageViewModel.cs
+++ ViewModels/ProductPageViewModel.cs
@@ -1,8 +1,8 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Linq;
 using GardenCentreApp.Models;
-using GardenCentreApp.Pages;
 
 namespace GardenCentreApp.ViewModels
 {
@@ -11,46 +11,67 @@ namespace GardenCentreApp.ViewModels
         [ObservableProperty]
         private ObservableCollection<Product> products;
 
-        public ObservableCollection<BasketItem> Basket { get; private set; } = new();
-        
-        
+        [ObservableProperty]
+        private ObservableCollection<string> categories;
+
+        [ObservableProperty]
+        private string selectedCategory;
+
         public ProductPageViewModel()
         {
+            // Initialize categories
+            Categories = new ObservableCollection<string>
+            {
+                "All",
+                "Plants",
+                "Tools",
+                "Garden Care",
+                "Seeds",
+                "Fertilizers"
+            };
+
             // Load all products by default
-            Products = new ObservableCollection<Product>(App.Database.GetProducts());
+            LoadProducts("All");
         }
 
-        public ProductPageViewModel(string category)
+        partial void OnSelectedCategoryChanged(string value)
         {
-            Products = new ObservableCollection<Product>(
-                App.Database.GetProducts().Where(p => p.Category == category)
-            );
+            if (!string.IsNullOrEmpty(value))
+            {
+                LoadProducts(value);
+            }
+        }
+
+        private void LoadProducts(string category)
+        {
+            if (category == "All")
+            {
+                Products = new ObservableCollection<Product>(App.Database.GetProducts());
+            }
+            else
+            {
+                Products = new ObservableCollection<Product>(
+                    App.Database.GetProducts().Where(p => p.Category == category)
+                );
+            }
         }
 
         [RelayCommand]
         private void AddToBasket(Product product)
         {
             var userId = Preferences.Get("UserId", 0);
-            var isCorporateClient = Preferences.Get("IsCorporateClient", false); // NEW: Get corporate flag
+            var isCorporateClient = Preferences.Get("IsCorporateClient", false);
 
             var basketItem = new BasketItem
             {
                 UserId = userId,
                 ProductId = product.Id,
                 Quantity = 1,
-                IsCorporatePurchase = isCorporateClient // NEW: Mark as corporate purchase if applicable
+                IsCorporatePurchase = isCorporateClient
             };
 
             App.Database.AddBasketItem(basketItem);
             Application.Current.MainPage.DisplayAlert("Added to Basket", $"{product.Name} has been added to your basket.", "OK");
-        }
-
-
-        [RelayCommand]
-        private async void ViewBasket()
-        {
-            var userId = Preferences.Get("UserId", 0);
-            await Application.Current.MainPage.Navigation.PushAsync(new BasketPage(userId));
         }
     }
 }
